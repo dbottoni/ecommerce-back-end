34e49709e2b61b4e428a6efccb72e8510a22e940
Dana Bottoni <danabottoni@gmail.com>
2021-08-15 12:55:51 -0400

    finish product routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -9,17 +9,15 @@ router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
   Product.findAll({
-    attributes: [
-      'id',
-      'product_name',
-      'price',
-      'stock',
-      'category_id'
-    ],
+    attributes: ['id','product_name','price','stock','category_id' ],
     include: [
       {
         model: Category,
         attributes: ['id', 'category_name']
+      },
+      {
+        model: Tag,
+        attributes: ['id', 'tag_name']
       }
     ]
   })
@@ -38,17 +36,15 @@ router.get('/:id', (req, res) => {
     where: {
       id: req.params.id
     },
-    attributes: [
-      'id',
-      'product_name',
-      'price',
-      'stock',
-      'category_id'
-    ],
+    attributes: ['id','product_name', 'price','stock','category_id'],
     include: [
       {
         model: Category,
         attributes: ['id', 'category_name'] 
+      },
+      {
+        model: Tag,
+        attributes: ['id', 'tag_name']
       }
     ]
   })
@@ -135,6 +131,22 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(dbProductData => {
+    id(!dbProductData){
+      res.status(404).json({ message : 'No product found with this ID'});
+      return;
+    }
+    res.json(dbProductData);
+  })
+  .catahc(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 module.exports = router;
