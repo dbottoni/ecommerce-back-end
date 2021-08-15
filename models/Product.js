141920bf794dc4334fecb9c6103dd785fa3619e7
Dana Bottoni <danabottoni@gmail.com>
2021-08-15 15:20:38 -0400

    added validations to Productjs file

--- models/Product.js
+++ models/Product.js
@@ -30,7 +30,10 @@ Product.init(
     stock: {
       type: DataTypes.INTEGER,
       allowNull: false,
-      defaultValue: 10, 
+      defaultValue: 10,
+      validate: {
+        isNumeric: true
+      } 
     },
     category_id: {
       type: DataTypes.INTEGER,
